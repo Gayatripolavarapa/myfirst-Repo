b50f7918327159ecc4cb6819f1fcde78d8fe71f2
Gayathri <githubpolavarapa17>
2026-03-06 12:28:46 +0530

    Base conversations

---
+++ Intro/ex.py
@@ -0,0 +1,21 @@
+print('Hello world')
+a=10
+print(a)
+print(id(a))
+print(a.__sizeof__())
+a=34+3j
+print(type(a))
+s1='Gayatri'
+s2="Kiwi"
+s3=''' Hi 
+I'm doing good'''
+print(s3)
+print(bin(10))
+print(oct(10))
+print(hex(10))
+print(int(0b1010))
+print(type(bool(10)))
+print(False+1)
+
+
+
